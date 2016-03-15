0bcf42a3663ae2e1bbf5bd7036376f8fd4e07ddf
ianngiaw <ianngiawtingan@gmail.com>
2016-03-15 16:45:31 +0800

    Added heuristic calculation and command line arguments for weights

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,29 +1,44 @@
 
 public class PlayerSkeleton {
+	private static double[] DEFAULT_WEIGHTS = {
+		-0.510066, // Aggregate column heights
+		-0.184483, // Bumpiness
+		0, // Max height
+		-0.6, // Num of holes created
+		0.760666 // Num of completed rows
+	};
+	private static double[] weights;
 
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
-		return evaluateMoves(s, legalMoves);
-	}
-
-	public int evaluateMoves(State s, int[][] moves) {
 		int move = 0;
 		double max = -Double.MAX_VALUE;
-		for(int i = 0; i < moves.length; i++) {
-			double result = scoreMove(s, moves[i][State.ORIENT], moves[i][State.SLOT]);
-			if (result > max) {
-				max = result;
-				move = i;
+		for(int i = 0; i < legalMoves.length; i++) {
+			try {
+				int[] results = scoreMove(s, legalMoves[i][State.ORIENT], legalMoves[i][State.SLOT]);
+				double score = 0;
+				for(int j = 0; j < weights.length; j++) {
+					score += results[j] * weights[j];
+				}
+				if (score > max) {
+					max = score;
+					move = i;
+				}
+			} catch (Exception e) {
+				continue;
 			}
 		}
 		return move;
 	}
 
-	public double scoreMove(State s, int orient, int slot) {
+	public int[] scoreMove(State s, int orient, int slot) throws Exception {
+		int[] results = new int[5];
+
 		int[][] field = s.getField();
 		int nextPiece = s.getNextPiece();
 		int turn = s.getTurnNumber() + 1;
-		int[] top = s.getTop();
+		int[] top = new int[s.getTop().length];
+		System.arraycopy(s.getTop(), 0, top, 0, s.getTop().length);
 		int[][][] pBottom = s.getpBottom();
 		int[][][] pTop = s.getpTop();
 		int[][] pHeight = s.getpHeight();
@@ -37,7 +52,7 @@ public class PlayerSkeleton {
 		}
 
 		if(height+pHeight[nextPiece][orient] >= State.ROWS) {
-			return -Double.MAX_VALUE;
+			throw new Exception();
 		}
 
 		//for each column in the piece - fill in the appropriate blocks
@@ -53,12 +68,38 @@ public class PlayerSkeleton {
 			top[slot+c]=height+pTop[nextPiece][orient][c];
 		}
 
-		int rowsCleared = 0;
+		results[0] = 0;
+		results[2] = 0;
+		// Sum heights and find max height
+		for(int c = 0; c < State.COLS; c++) {
+			results[0] += top[c];
+			results[2] = top[c] > results[2] ? top[c] : results[2];
+		}
+		results[1] = 0;
+		// Find bumpiness
+		for(int c = 0; c < State.COLS - 1; c++) {
+			results[1] += Math.abs(top[c] - top[c+1]);
+		}
+		results[3] = 0;
+		// Find new holes
+		for(int i = 0; i < pWidth[nextPiece][orient]; i++) {
+			//from bottom to top of brick
+			int colHoles = 0;
+			for(int h = height+pBottom[nextPiece][orient][i]; h >= 0; h--) {
+				if (colHoles > 0 && field[h][i+slot] != 0) break;
+				if (field[h][i+slot] == 0) {
+					colHoles++;
+				}
+			}
+			results[3] += colHoles;
+		}
+
+		results[4] = 0;
 		//check for full rows - starting at the top
 		for(int r = height+pHeight[nextPiece][orient]-1; r >= height; r--) {
 			//check all columns in the row
 			boolean full = true;
-			for(int c = 0; c < COLS; c++) {
+			for(int c = 0; c < State.COLS; c++) {
 				if(field[r][c] == 0) {
 					full = false;
 					break;
@@ -66,13 +107,8 @@ public class PlayerSkeleton {
 			}
 			//count possibly cleared rows
 			if(full) {
-				rowsCleared++;
+				results[4]++;
 			}
-		}
-
-		//re-adjust top
-		for(int c = 0; c < pWidth[nextPiece][orient]; c++) {
-			top[slot+c]=height-pTop[nextPiece][orient][c];
 		}
 
 		//for each column in the piece - empty the appropriate blocks
@@ -82,9 +118,46 @@ public class PlayerSkeleton {
 				field[h][i+slot] = 0;
 			}
 		}
-		return field;
+		return results;
+	}
+
+	// Modified main method
+	public static void main(String[] args) {
+		// Init weights
+		if (args.length != DEFAULT_WEIGHTS.length) {
+			weights = DEFAULT_WEIGHTS;
+		} else {
+			weights = new double[DEFAULT_WEIGHTS.length];
+			for(int i = 0; i < DEFAULT_WEIGHTS.length; i++) {
+				try {
+					weights[i] = Double.parseDouble(args[i]);
+				} catch (NumberFormatException e) {
+					weights = DEFAULT_WEIGHTS;
+					break;
+				}
+			}
+		}
+
+		State s = new State();
+		new TFrame(s);
+		PlayerSkeleton p = new PlayerSkeleton();
+		while(!s.hasLost()) {
+			s.makeMove(p.pickMove(s,s.legalMoves()));
+			s.draw();
+			s.drawNext(0,0);
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		// Prints the number of rows cleared and the turn number
+		System.out.println(s.getRowsCleared() + " " + s.getTurnNumber());
+		System.exit(0);
 	}
 	
+	// Default main method
+	/*
 	public static void main(String[] args) {
 		State s = new State();
 		new TFrame(s);
@@ -100,6 +173,5 @@ public class PlayerSkeleton {
 			}
 		}
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
-	}
-	
+	}*/
 }
