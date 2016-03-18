32fdb4b5ba93f4fbf763fc56cc432c2eec992d26
cwma <chiaweimeng.alexander@gmail.com>
2016-03-18 15:14:26 +0800

    Added MultiProcessing, Modified parents generation
    Added multiprocessing pools for calling the java program. Set pool_size
    to the number of logical cpu's on your laptop/desktop.
    
    Changed the way parents are generated from the initial seed, with new
    weights being generated from a delta of a seed weight of 0.1. Mutations
    are also handled this way.

--- AlphaTetris.py
+++ AlphaTetris.py
@@ -3,10 +3,24 @@
 import subprocess
 import sys
 import random
+import signal
+import operator
+from math import floor
+from multiprocessing import Pool
+
+# have to keep the rungame process in a method outside the class due to how multiprocessing pool works
+def runGame(weights):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    command = ["java", "PlayerSkeleton"] + [str(x) for x in weights]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    numRowsCleared, numTurns = process.stdout.read().decode("utf-8").strip("\r\n").split(" ")
+    return (int(numRowsCleared), numTurns)
 
 class AlphaTetris():
     """Genetic Algorithm to optimise weights for heuristics in PlayerSkeleton.java"""
 
+    pool_size = 4
     defaultWeights = [
         -0.510066, # Aggregate column heights
         -0.184483, # Bumpiness
@@ -16,40 +30,43 @@ class AlphaTetris():
     ]
 
     def __init__(self):
-        self.populationCount = 4    # Must be even
+        self.pool = Pool(AlphaTetris.pool_size)
+        self.populationCount = 32    # Must be even
         self.mutationRate = 0.05    # [0, 1]
         self.generation = 1
-        self.start([AlphaTetris.defaultWeights] + [self.generateWeights() for i in range(self.populationCount - 1)])
+        self.start([AlphaTetris.defaultWeights] + [self.generateWeights(AlphaTetris.pool_size) for i in range(self.populationCount - 1)])
 
     def start(self, populationWeights):
         print("Generation", self.generation)
 
         choices = []
-        for weights in populationWeights:
-            print("Weights used:", weights)
-            numRowsCleared, numTurns = self.runGame(weights)
-            choices.append((weights, int(numRowsCleared)))
-            print("Rows cleared:", numRowsCleared, "Number of turns:", numTurns)
+        pool = Pool(self.pool_size)
 
-        parents = self.selectParents(choices)
-        self.crossover(parents)
-        self.mutate(parents)
+        try:
+            scores = pool.map(runGame, populationWeights)
+        except KeyboardInterrupt as ki:
+            pool.join()
+            pool.terminate()
+        else:
+            for (numRowsCleared, numTurns), weights in zip(scores, populationWeights):
+                print("Weights used:", weights)
+                print("Rows cleared:", numRowsCleared, "Number of turns:", numTurns)
+                choices.append((weights, numRowsCleared))
 
-        self.generation += 1
-        self.start(parents)
+            parents = self.selectParents(choices)
+            self.crossover(parents)
+            self.mutate(parents)
 
-    def runGame(self, weights):
-        command = ["java", "PlayerSkeleton"] + [str(x) for x in weights]
-        process = subprocess.run(command, stdout=subprocess.PIPE)
-        return process.stdout.decode("utf-8").split(" ")
-
+            self.generation += 1
+            self.start(parents)
 
     #### Initial population
-    def generateWeights(self):
-        return [self.generateWeight() for i in range(len(AlphaTetris.defaultWeights))]
+    def generateWeights(self, seed):
+        return [self.generateWeight(i) for i in AlphaTetris.defaultWeights]
 
-    def generateWeight(self):
-        return random.uniform(-1, 1)
+    def generateWeight(self, origin):
+        return origin + random.uniform(-0.1, 0.1)
+
 
 
     #### Selection
@@ -68,7 +85,6 @@ class AlphaTetris():
 
         assert False, "Shouldn't get here"
 
-
     #### Crossover
     def crossover(self, parents):
         for i in range(0, self.populationCount, 2):
@@ -82,7 +98,8 @@ class AlphaTetris():
     def mutate(self, parents):
         for parent in parents:
             if random.uniform(0, 1) < self.mutationRate:
-                parent[random.randint(0, len(AlphaTetris.defaultWeights) - 1)] = self.generateWeight()
+                mutation_target = random.randint(0, len(AlphaTetris.defaultWeights) - 1)
+                parent[mutation_target] = self.generateWeight(parent[mutation_target])
 
 
 
